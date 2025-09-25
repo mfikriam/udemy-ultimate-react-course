b7587b79b45fec1258de7c46c2db13682f0e03a5
mfikriam <mfikriam.pro@gmail.com>
2025-09-25 08:25:55 +0800

    219. Programmatic Navigation with <Navigate />

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
@@ -11,6 +11,7 @@ import PageNotFound from './pages/PageNotFound';
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
 import City from './components/City';
+import Form from './components/Form';
 
 const BASE_URL = 'http://localhost:8000';
 
@@ -47,11 +48,11 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route path="cities/:id" element={<City />} />
           <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
