580586b41ba92f465f1b29434d14f63bf6152f7a
mfikriam <mfikriam.pro@gmail.com>
2025-09-29 09:32:21 +0800

    252. Back to The "WorldWise" App

--- 11-worldwise/src/contexts/CitiesContext.jsx
+++ 11-worldwise/src/contexts/CitiesContext.jsx
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import { createContext, useEffect, useReducer } from 'react';
 
 const BASE_URL = 'http://localhost:8000';
@@ -80,19 +81,22 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
-    if (Number(id) === currentCity.id) return;
+  const getCity = useCallback(
+    async function getCity(id) {
+      if (Number(id) === currentCity.id) return;
 
-    dispatch({ type: 'loading' });
+      dispatch({ type: 'loading' });
 
-    try {
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
-      dispatch({ type: 'city/loaded', payload: data });
-    } catch {
-      dispatch({ type: 'rejected', payload: 'There was an error loading data...' });
-    }
-  }
+      try {
+        const res = await fetch(`${BASE_URL}/cities/${id}`);
+        const data = await res.json();
+        dispatch({ type: 'city/loaded', payload: data });
+      } catch {
+        dispatch({ type: 'rejected', payload: 'There was an error loading data...' });
+      }
+    },
+    [currentCity.id],
+  );
 
   async function createCity(newCity) {
     dispatch({ type: 'loading' });
