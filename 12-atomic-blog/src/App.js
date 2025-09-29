0f61311311cc2c5f534f7ed80fef5fea3c4feac1
mfikriam <mfikriam.pro@gmail.com>
2025-09-29 09:16:19 +0800

    251. Optimizing Context Re-Renders

--- 12-atomic-blog/src/App.js
+++ 12-atomic-blog/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 import { faker } from '@faker-js/faker';
 import { PostProvider, usePostContext } from './PostContext';
 // import Test from './Test';
@@ -76,14 +76,14 @@ function Results() {
   return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
-function Main() {
+const Main = memo(function Main() {
   return (
     <main>
       <FormAddPost />
       <Posts />
     </main>
   );
-}
+});
 
 function Posts() {
   return (
