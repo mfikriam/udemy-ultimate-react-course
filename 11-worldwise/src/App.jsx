31f4490093a2c564639d658242c62abf150628ec
mfikriam <mfikriam.pro@gmail.com>
2025-09-29 10:25:08 +0800

    254. Don't Optimize Prematurely!

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,15 +1,27 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
 import { CitiesProvider, FakeAuthProvider } from './contexts';
 import {
-  AppLayout,
-  Homepage,
-  Login,
-  PageNotFound,
-  Pricing,
-  Product,
+  // AppLayout,
+  // Homepage,
+  // Login,
+  // PageNotFound,
+  // Pricing,
+  // Product,
   ProtectedRoute,
 } from './pages';
-import { CityList, CountryList, City, Form } from './components';
+import { CityList, CountryList, City, Form, SpinnerFullPage } from './components';
+
+const AppLayout = lazy(() => import('./pages/AppLayout'));
+const Homepage = lazy(() => import('./pages/Homepage'));
+const Login = lazy(() => import('./pages/Login'));
+const PageNotFound = lazy(() => import('./pages/PageNotFound'));
+const Pricing = lazy(() => import('./pages/Pricing'));
+const Product = lazy(() => import('./pages/Product'));
+
+// dist/assets/index-5c973157.css   30.50 kB │ gzip:   5.13 kB
+// dist/assets/index-2eb7f67d.js   512.64 kB │ gzip: 149.68 kB
 
 function App() {
   return (
@@ -21,27 +33,29 @@ function App() {
             v7_relativeSplatPath: true,
           }}
         >
-          <Routes>
-            <Route index element={<Homepage />} />
-            <Route path="product" element={<Product />} />
-            <Route path="pricing" element={<Pricing />} />
-            <Route path="login" element={<Login />} />
-            <Route
-              path="app"
-              element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
-              }
-            >
-              <Route index element={<Navigate replace to="cities" />} />
-              <Route path="cities" element={<CityList />} />
-              <Route path="cities/:id" element={<City />} />
-              <Route path="countries" element={<CountryList />} />
-              <Route path="form" element={<Form />} />
-            </Route>
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route index element={<Homepage />} />
+              <Route path="product" element={<Product />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="login" element={<Login />} />
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Navigate replace to="cities" />} />
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </CitiesProvider>
     </FakeAuthProvider>
