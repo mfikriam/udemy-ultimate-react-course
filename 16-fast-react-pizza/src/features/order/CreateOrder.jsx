3d23ff5398ecac1ba7cd7351fba39117fe54b4f1
mfikriam <mfikriam.pro@gmail.com>
2025-10-02 17:02:43 +0800

    292. Error Handling in Form Actions

--- 16-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,4 +1,4 @@
-import { Form, redirect } from 'react-router-dom';
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { useState } from 'react';
 import { createOrder } from '../../services/apiRestaurant';
 
@@ -30,6 +30,11 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
+  const formErrors = useActionData();
+
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
@@ -49,6 +54,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -71,7 +77,7 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button>Order now</button>
+          <button disabled={isSubmitting}>{isSubmitting ? 'Placing order...' : 'Order now'}</button>
         </div>
       </Form>
     </div>
@@ -88,6 +94,13 @@ export async function action({ request }) {
     priority: data.priority === 'on',
   };
 
+  const errors = {};
+  if (!isValidPhone(order.phone)) {
+    errors.phone = 'Please give us your correct phone number. We might need it to contact you.';
+  }
+  if (Object.keys(errors).length > 0) return errors;
+
+  // If everything is okay, create new order and redirect
   const newOrder = await createOrder(order);
 
   return redirect(`/order/${newOrder.id}`);
