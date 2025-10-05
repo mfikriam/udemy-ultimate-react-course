b24427c9e53b24ded82cb7cad635e5f4fc8d7ba3
mfikriam <mfikriam.pro@gmail.com>
2025-10-05 20:42:32 +0800

    328. Setting Up the Project: "The Wild Oasis"

---
+++ 17-the-wild-oasis/vite.config.js
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+
+export default defineConfig({
+  plugins: [react(), eslint()],
+});
