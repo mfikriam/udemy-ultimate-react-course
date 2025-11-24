28f3ac269529c289aec41629a6176d9f5ef55929
mfikriam <mfikriam.pro@gmail.com>
2025-11-24 19:59:20 +0800

    332. Building More Reusable Styled Components

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -2,15 +2,11 @@ import styled from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 import Button from './ui/Button';
 import Input from './ui/Input';
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
+import Heading from './ui/Heading';
+import Row from './ui/Row';
 
 const StyledApp = styled.main`
-  background-color: orangered;
+  /* background-color: orangered; */
   padding: 20px;
 `;
 
@@ -19,12 +15,27 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>The Wild Oasis</H1>
-        <Button onClick={() => alert('Check in')}>Check in</Button>
-        <Button onClick={() => alert('Check out')}>Check out</Button>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
 
-        <Input type="number" placeholder="Number of guests" />
-        <Input type="number" placeholder="Number of guests" />
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert('Check in')}>Check in</Button>
+              <Button variation="secondary" size="small" onClick={() => alert('Check out')}>
+                Check out
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
