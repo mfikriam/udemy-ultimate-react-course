628ff163d34bd7ece0c25662d99af1587f1a534a
mfikriam <mfikriam.pro@gmail.com>
2025-12-02 16:15:44 +0800

    349. Fetching Cabin Data

--- 17-the-wild-oasis/src/utils/helpers.js
+++ 17-the-wild-oasis/src/utils/helpers.js
@@ -1,5 +1,4 @@
-import { formatDistance, parseISO } from 'date-fns';
-import { differenceInDays } from 'date-fns/esm';
+import { differenceInDays, formatDistance, parseISO } from 'date-fns';
 
 // We want to make this function work for both Date objects and strings (which come from Supabase)
 export const subtractDates = (dateStr1, dateStr2) =>
@@ -25,6 +24,4 @@ export const getToday = function (options = {}) {
 };
 
 export const formatCurrency = (value) =>
-  new Intl.NumberFormat('en', { style: 'currency', currency: 'USD' }).format(
-    value
-  );
+  new Intl.NumberFormat('en', { style: 'currency', currency: 'USD' }).format(value);
