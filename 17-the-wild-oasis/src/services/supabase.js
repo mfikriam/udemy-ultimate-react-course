59c6dd70ee53220e60e6a20918e926b7d6dc30c8
mfikriam <mfikriam.pro@gmail.com>
2025-12-03 20:25:51 +0800

    355. Uploading Images to Supabase

--- 17-the-wild-oasis/src/services/supabase.js
+++ 17-the-wild-oasis/src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xjxrsynfkriuwplqenwl.supabase.co';
+export const supabaseUrl = 'https://xjxrsynfkriuwplqenwl.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqeHJzeW5ma3JpdXdwbHFlbndsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1MTk0NTgsImV4cCI6MjA4MDA5NTQ1OH0.L834l78h2dIG5ESVB_ahhOVwctvKVrzgiAo2kCHNHcs';
 const supabase = createClient(supabaseUrl, supabaseKey);
