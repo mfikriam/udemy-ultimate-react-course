67d187913ada8b5124edd239ba33e44bbdad277f
mfikriam <mfikriam.pro@gmail.com>
2025-12-15 16:47:07 +0800

    383. Prefetching With React Query

--- 17-the-wild-oasis/src/features/bookings/BookingTable.jsx
+++ 17-the-wild-oasis/src/features/bookings/BookingTable.jsx
@@ -8,7 +8,7 @@ import Spinner from '../../ui/Spinner';
 import Pagination from '../../ui/Pagination';
 
 function BookingTable() {
-  const { bookings, isLoading } = useBookings();
+  const { bookings, isLoading, count } = useBookings();
 
   if (isLoading) return <Spinner />;
 
@@ -29,7 +29,7 @@ function BookingTable() {
         <Table.Body data={bookings} render={(booking) => <BookingRow key={booking.id} booking={booking} />} />
 
         <Table.Footer>
-          <Pagination count={45} />
+          <Pagination count={count} />
         </Table.Footer>
       </Table>
     </Menus>
