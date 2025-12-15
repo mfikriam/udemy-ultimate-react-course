84b4ea6a861fd6994a2118710428855b82a95eec
mfikriam <mfikriam.pro@gmail.com>
2025-12-15 11:06:54 +0800

    377. Building the Bookings Table

--- 17-the-wild-oasis/src/features/bookings/BookingTable.jsx
+++ 17-the-wild-oasis/src/features/bookings/BookingTable.jsx
@@ -1,9 +1,17 @@
-import BookingRow from "./BookingRow";
-import Table from "../../ui/Table";
-import Menus from "../../ui/Menus";
+import { useBookings } from './useBookings';
+
+import BookingRow from './BookingRow';
+import Table from '../../ui/Table';
+import Menus from '../../ui/Menus';
+import Empty from '../../ui/Empty';
+import Spinner from '../../ui/Spinner';
 
 function BookingTable() {
-  const bookings = [];
+  const { bookings, isLoading } = useBookings();
+
+  if (isLoading) return <Spinner />;
+
+  if (!bookings.length) return <Empty resourceName="bookings" />;
 
   return (
     <Menus>
@@ -17,12 +25,7 @@ function BookingTable() {
           <div></div>
         </Table.Header>
 
-        <Table.Body
-          data={bookings}
-          render={(booking) => (
-            <BookingRow key={booking.id} booking={booking} />
-          )}
-        />
+        <Table.Body data={bookings} render={(booking) => <BookingRow key={booking.id} booking={booking} />} />
       </Table>
     </Menus>
   );
